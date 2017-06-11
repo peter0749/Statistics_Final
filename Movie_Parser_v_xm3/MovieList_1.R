ad766f511e04adc32f8c5800759c2428ab95bea8
xm35p4fu6 <xm35p4fu6@gmail.com>
2017-06-11 11:47:53 +0800

    add xm3 version

---
+++ Movie_Parser_v_xm3/MovieList_1.R
@@ -0,0 +1,64 @@
+require(XML)
+require(bitops)
+require(RCurl)
+require(httr)
+
+rm(list=ls(all=TRUE))
+yahoourl = "http://www.boxofficemojo.com/yearly/chart/?page="
+appendurl_1 = "&view=releasedate&view2=domestic&yr="
+appendurl_2 = "&p=.htm"
+testurl = ""
+testvector = c()
+testframe = data.frame()
+
+myHttpheader<- c(
+  "User-Agent"="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+  "Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+  "Connection"="keep-alive",
+  "Accept-Charset"="big5,GB2312,utf-8;q=0.7,*;q=0.7",
+  "Accept-Encoding"="gzip, deflate, sdch",
+  "Accept-Language"="zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4",
+  "Upgrade-Insecure-Requests"="1",
+  "Cache-Control"="max-age=0",
+  "Cookie"="__utmt=1; __utma=137419939.1443367072.1468586224.1468720076.1468727749.10; __utmb=137419939.13.10.1468727749; __utmc=137419939; __utmz=137419939.1468600072.4.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)",
+  "Host"="www.boxofficemojo.com"
+)
+
+for(j in 1980:2016)
+{
+  for(i in 1:5)
+  {
+    testurl = paste(yahoourl,i,appendurl_1,j,appendurl_2,sep='')
+    testexist = url.exists(testurl)
+    print(testurl)
+    print(testexist)
+    if(testexist)
+    {
+      html = getURL(testurl, ssl.verifypeer = FALSE, encoding='UTF-8', httpheader = myHttpheader)
+      xml = htmlParse(html, encoding='UTF-8')
+      path = xpathSApply(xml,'//table//td[2]/b/font[@size="2"]/a/@href',sessionEncoding='UTF-8')
+      if(length(path)<1) break
+      title = xpathSApply(xml,'//table//td[2]/b/font[@size="2"]/a[@href]',sessionEncoding='UTF-8',xmlValue)
+      if(length(title)<1) break
+      box = xpathSApply(xml,'//table//td[4]/font[@size="2"]/b',sessionEncoding='UTF-8',xmlValue)
+      if(length(box)<1) break
+      
+      testlen = length(path)
+      
+      if(length(title)!=testlen || length(box)!=testlen) next
+      tempframe = data.frame(title,path,box)
+      testframe = rbind(testframe,tempframe)
+    }
+    else print(paste("assert: URL:",testurl,"not exist!"))
+  }
+}
+
+names(testframe) = c("Title","Path","Box")
+
+#Post-processing and sorting
+testframe$Box = substring(testframe$Box,2)
+testframe$Box = gsub(",","",testframe$Box)
+testframe$Box = as.numeric(testframe$Box)
+testframe = testframe[order(testframe$Box,decreasing=TRUE),]
+
+write.csv(testframe,"testcsv.csv")
---
+++ Movie_Parser_v_xm3/SocialNet_3.R
@@ -0,0 +1,50 @@
+rm(list=ls(all=TRUE))
+library(XML)
+library(bitops)
+library(RCurl)
+library(NLP)
+library(httr)
+library(chron)
+require(Rfacebook)
+
+tok = 'EAACEdEose0cBAEoW2eO8GqKHz7zGiXDtm29CszRk4FAlVMOsACESjKBuZAnmirw2u5HNNrDEHA7LuKZCdItM67QNitW4qPEPuGXhAFAGMiMc5Tp9vQqbzsHczJ6Ce0s2eamPV0CjZBe2QS9ccrLvOFx9LZA2Q7HmDyifonjspAhHK33OFH7ZCWjZBcVCFXVWgZD'
+
+alldata = read.csv("Fulllist.csv")
+
+youtubeSRC = 'https://www.youtube.com/results?q='
+yAppendURL = '%20trailer&sp=CAA%253D'
+youtubeURL = ''
+
+fulldata = data.frame()
+
+myHttpheader<- c(
+  "User-Agent"="Chrome/51.0.2704.103",
+  "Upgrade-Insecure-Requests"="1"
+)
+
+for( i in 1:length(alldata$X))
+{
+  testframe = data.frame( 'FB_likes' = Rfacebook::searchPages(alldata$Title[i], tok, n=1)$likes , 'YoutubeViews'=NA)
+  youtubeURL <- paste(youtubeSRC, alldata$Title[i], yAppendURL, sep='')
+  youtubeURL <- gsub(" ","%20",youtubeURL)
+  #youtubeURL <- iconv(youtubeURL, "big5", "utf8")
+  #Encoding(youtubeURL) = "UTF-8"
+  print(youtubeURL)
+  URLExist = url.exists(youtubeURL)
+  print(URLExist)
+  if(URLExist)
+  {
+    html = getURL(youtubeURL, ssl.verifypeer = FALSE, encoding='UTF-8', httpheader = myHttpheader)
+    xml = htmlParse(html, encoding='UTF-8')
+    text = xpathSApply(xml,'//li/div/div/div[2]/div[2]/ul/li[2]/text()', sessionEncoding='utf8', xmlValue)
+    if(length(text)<1) next
+    text = substring(text,6)
+    text <- gsub(",","",text)
+    text <- as.numeric(text)
+    testframe$'YoutubeViews' = t(text)[1]
+    testframe = cbind(alldata[i,-1],testframe)
+    fulldata = rbind(fulldata, testframe)
+  }
+}
+
+write.csv(fulldata,"Youtubelist.csv")
